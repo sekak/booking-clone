9661c337c5d1ce73d0e2e493cfb3a3795f22b5c2
sekak <ahmadesekak@gmail.com>
2024-12-03 10:19:12 +0100

    cors urls

--- backend-nodejs/index.js
+++ backend-nodejs/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ['https://booking-clone-cmve.vercel.app/', 'http://localhost:3000'],
+    origin: ['https://booking-clone-3.onrender.com', 'http://localhost:3000'],
     credentials: true,
   })
 );
